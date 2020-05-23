6fbaa44689ff925bfcacac18d7fb27ccb247eee1
pengboyun <2147217957@qq.com>
2020-05-23 22:45:41 +0800

    1

---
+++ Self_Aught/class1No2/main.cpp
@@ -0,0 +1,30 @@
+//
+//  main.cpp
+//  Class1No2
+//
+//  Created by 彭博云 on 2020/5/8.
+//  Copyright © 2020 彭博云. All rights reserved.
+//
+
+#include <iostream>
+#include <stdio.h>
+void two_1(){
+    int n;
+    scanf("%d",&n);
+    int i;
+    for(i = 1; i <= n; i++){
+        printf("%d\n", i);
+}
+}//
+    void Example(){
+    for (int a = 1; a <= 9; a++) {
+        for (<#initialization#>; <#condition#>; <#increment#>) {
+            <#statements#>
+        }
+    }
+}
+
+int main() {
+    two_1();
+    return 0;
+}
