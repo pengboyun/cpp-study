7655bb7b369afa2559738c5ecc7c67fbd4d2fcee
pengboyun <2147217957@qq.com>
2021-05-12 22:22:44 +0800

    run

--- Homework/Work29/main.cpp
+++ Homework/Work29/main.cpp
@@ -3,23 +3,12 @@ using namespace std;
 
 
 int main(){
-    char a[1000]={},b[1000]={};
-    int a1[1000]={},b1[1000]={},c1[1000]={};
-    cin >> a >> b;
-    long lena=strlen(a),lenb=strlen(b);
-    long len = (lena > lenb?lena:lenb);
-    
-    for(int i = 0;i < lena;i++)
-        a1[i] = a[lena - 1 - i]-48;
-    
-    for(int i = 0;i < lenb;i++)
-        b1[i] = b[lenb - 1 -i]-48;
-    
-    for (int i = 0;i<len;i++)
-        c1[i] = b1[i] + a1[i];
-    
-    
-    for (int i = 0; i < len; i++)
-        printf("%d",c1[len - 1 - i]);
-    printf("\n");}
-
+    float k,out;
+    for (int i = 0; i <= 99; i++) {
+        cin k
+        if(){
+            
+        }
+    }
+}
+w
--- Homework/paixu1/main.cpp
+++ Homework/paixu1/main.cpp
@@ -1,38 +1,11 @@
 #include<iostream>
 using namespace std;
-const int maxn = 101;
-int a[maxn],r[maxn];
-void merge(int left,int right){
 
-    if(left == right)
-        return;
-    int mid = (left+right)/2;
-    merge(left,mid);
-    merge(mid+1,right);
-    int i = left, j = mid+1;
-    int k = left;
-    while(i <= mid && j<= right){
-        if( a[i]<=a[j] )
-            r[k++] = a[i++];
-        else
-            r[k++] = a[j++];
-    }
-    while( i<=mid )
-        r[k++]=a[i++];
-    while( j<=right )
-        r[k++]=a[j++];
-    for(i=left;i<=right;i++) a[i] =r[i];
-
-    return;
-}
 int main(){
-    int n,i;
-    cin >> n;
-    for(i=1;i<=n;i++)
-        cin >> a[i];
-    merge(1,n);
-    for(i=1;i<=n;i++)
-        cout << a[i]<<' ';
+    int cnt = 0;
+    for(int i = 1000. i <= 9999;i++){
+        int
+    }
     return 0;
 }
 
