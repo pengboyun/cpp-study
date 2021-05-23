fe0528d88076216285b793556e1414c164b82e2e
pengboyun <2147217957@qq.com>
2021-05-23 12:07:12 +0800

    run

--- Homework/paixu1/main.cpp
+++ Homework/paixu1/main.cpp
@@ -2,10 +2,26 @@
 using namespace std;
 
 int main(){
-    int cnt = 0;
-    for(int i = 1000. i <= 9999;i++){
-        int
+    int g[101][101],vis[101],n,e,temp=0;
+    void bfs(int cur){
+        int head = 0,tail = 1;
+        q[0] = cur;
+        cout << cur << " ";
+        vir[cur] = 1;
+        while (head < tail) {
+            for (int i = 1; i <= n; i++) {
+                if (g[head][i] && vis[i] = 0) {
+                    <#statements#>
+                }
+                
+            }
+        }
     }
+    cin >> n >> e;
+    for(int i = 1;i <= e;i++){
+        
+        
+}
     return 0;
 }
 
